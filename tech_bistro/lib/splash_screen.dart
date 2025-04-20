0d1b6ecbb91dd68490e96dd07e3c52ca3715ecc1
Bruno Campagnol <bruno_unky@hotmail.com>
2025-04-20 16:33:15 -0300

    Tela de carregamento
    Falta adicionar um icone de loop de carregamento

---
+++ tech_bistro/lib/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'main.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const MainApp()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/logo_branco.svg',
+          width: 150,
+        ),
+      ),
+    );
+  }
+}

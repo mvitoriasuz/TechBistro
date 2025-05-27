9ab720f7df0383742589bc00df7a2a177f0b1a91
Bruno Campagnol <bruno_unky@hotmail.com>
2025-05-27 20:00:56 -0300

    Conexao com o Supabase

--- tech_bistro/lib/main.dart
+++ tech_bistro/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';  
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://hliczkulyvskjjbigvvk.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsaWN6a3VseXZza2pqYmlndnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzgyMTEsImV4cCI6MjA2MjQxNDIxMX0.qexOzbr1wBH6D07pk2wgAJTI1GidrAXrpMZSZzl-0NE',
+  );
+
   runApp(const MyApp());
 }
 

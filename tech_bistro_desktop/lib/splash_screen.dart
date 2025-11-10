dfe30201e6295115310cbfe223b1cb090e6537a3
Maria Vitoria <maria.suzarth@hotmail.com>
2025-11-10 10:59:13 -0300

    Inicio desenvolvimento desktop #89

---
+++ tech_bistro_desktop/lib/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'src/features/usuario/presentation/usuario.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const UsuarioPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'TECHBISTRO DESKTOP',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+                fontFamily: 'Nats',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ tech_bistro_desktop/lib/src/features/usuario/presentation/usuario.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class UsuarioPage extends StatelessWidget {
+  const UsuarioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Usuario Page'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Usuario Page!'),
+      ),
+    );
+  }
+  
+}

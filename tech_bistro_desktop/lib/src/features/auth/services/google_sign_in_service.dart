66f9bee146ece601a7a7aecdd3d6e8f4eb4c8945
Maria Vitoria <maria.suzarth@hotmail.com>
2025-11-20 17:36:32 -0300

    Autenticação Backlog
    Autenticação com o google desativada por enquanto

--- tech_bistro_desktop/lib/src/features/auth/services/google_sign_in_service.dart
+++ tech_bistro_desktop/lib/src/features/auth/services/google_sign_in_service.dart
@@ -1,19 +1,19 @@
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:flutter/foundation.dart';
 
-class GoogleSignInService {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+// class GoogleSignInService {
+//   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Future<void> signIn() async {
-    try {
-      final result = await _googleSignIn.signIn();
-      if (result != null) {
-        debugPrint('Usuário logado: ${result.displayName}');
-      } else {
-        debugPrint('Login cancelado.');
-      }
-    } catch (e) {
-      debugPrint('Erro no login Google: $e');
-    }
-  }
-}
+//   Future<void> signIn() async {
+//     try {
+//       final result = await _googleSignIn.signIn();
+//       if (result != null) {
+//         debugPrint('Usuário logado: ${result.displayName}');
+//       } else {
+//         debugPrint('Login cancelado.');
+//       }
+//     } catch (e) {
+//       debugPrint('Erro no login Google: $e');
+//     }
+//   }
+// }

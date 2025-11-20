31b8b1d8e5240edf44d9860cdada0a1acb614fd4
Maria Vitoria <maria.suzarth@hotmail.com>
2025-11-20 17:38:52 -0300

    Teste com banco de homologação ref #89
    Foi realizado a criação de um banco de homologação para concluir as configurações de login, home, usuário e hierarquia

--- tech_bistro_desktop/lib/main.dart
+++ tech_bistro_desktop/lib/main.dart
@@ -12,8 +12,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://hliczkulyvskjjbigvvk.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsaWN6a3VseXZza2pqYmlndnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzgyMTEsImV4cCI6MjA2MjQxNDIxMX0.qexOzbr1wBH6D07pk2wgAJTI1GidrAXrpMZSZzl-0NE',
+    // url: 'https://hliczkulyvskjjbigvvk.supabase.co',
+    url: 'https://hvlpbzivywqfhpsonbrj.supabase.co',
+    // anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsaWN6a3VseXZza2pqYmlndnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MzgyMTEsImV4cCI6MjA2MjQxNDIxMX0.qexOzbr1wBH6D07pk2wgAJTI1GidrAXrpMZSZzl-0NE',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bHBieml2eXdxZmhwc29uYnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NDIyNjMsImV4cCI6MjA3OTIxODI2M30.4XMGrEtej8amw1DacuGiRUMHurOnFCawkXXaiEMui4k',
   );
     runApp(const MyApp());
 }
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       //depois precisa alterar para a tela de autenticação
-      home: HomePage(),
+      home: AuthScreen(),
     );
   }
 }
